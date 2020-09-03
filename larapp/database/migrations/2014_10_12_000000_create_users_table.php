9f2f98f0018595164821257babaafd650d6f6d4d
jhongmez7 <gmezjhon@gmail.com>
2020-09-02 20:11:28 -0500

    Migraciones creadas - categorias - juegos

--- larapp/database/migrations/2014_10_12_000000_create_users_table.php
+++ larapp/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,15 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->bigInteger('phone');
+            $table->date('birthdate');
+            $table->string('gender');
+            $table->string('address');
+            $table->string('photo')->default('imgs/no-photo.png');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('customer');
+            $table->boolean('active')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
